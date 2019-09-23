798a981ed5e590f2147ded92215b4b9ee3621b54
Rogith-Ranger <rogithy@gmail.com>
2019-09-23 20:59:36 +0530

    About Complete

---
+++ src/About.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './static/About.css'
+class About extends Component {
+    render() {
+        return (
+    <div className="container">
+        <div className="section animated bounceInLeft">
+            <div className="brandname">
+                <h3>CEG Tech Forum</h3>
+                <ul>
+                    <li> +91 1234567890</li>
+                    <li>contact@ctf.com</li>
+                    <li>www.cegtechforum.com</li>
+                </ul>
+            </div>
+            <div className="contact">
+                <h3>Email Me</h3>
+                <form action="#">
+                    <p>
+                        <label>Name</label>
+                        <input type="text" name="name"/>
+                    </p>
+                    <p>
+                        <label>Company</label>
+                        <input type="text" name="company"/>
+                    </p>
+                    <p>
+                        <label>Email Address</label>
+                        <input type="email" name="email"/>
+                    </p>
+                    <p>
+                        <label>Phone Number</label>
+                        <input type="text" name="phone"/>
+                    </p>
+                    <p className="full">
+                        <button>Submit</button>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+        )
+    }
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './static/Mystyles.css'
 import Footer from './Footer';
 import MyNavbar from './MyNavbar';
 import Mainpage from './Mainpage';
-
-function App() {
+class App extends React.Component {
+  state = {
+    link:""
+  }
+  getLink = (link) => {
+    this.setState({link:link})
+    }
+  render(){
   return (
     <div className="App">
-    <MyNavbar/>
-    <Mainpage/>
+    <MyNavbar clickedLink={this.getLink}/>
+    <Mainpage clickedLink={this.state.link}/>
     <Footer/>
     </div>
   );
 }
-
+}
 export default App;
