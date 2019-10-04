e1cb16352d63dfe2072d1f75ce1c3ee47bf9c91b
Rogith-Ranger <rogithy@gmail.com>
2019-10-04 20:14:08 +0530

    Updates

---
+++ precache-manifest.7d09cb8ac5af3901ef164993edb11ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437fb5c18c14a588002b131ea2a8d913",
+    "url": "/ctf_task/index.html"
+  },
+  {
+    "revision": "5fe29c2c4d7d67861c24",
+    "url": "/ctf_task/static/css/main.61f6dcfd.chunk.css"
+  },
+  {
+    "revision": "c433f050a33f1962da19",
+    "url": "/ctf_task/static/js/2.041e56c1.chunk.js"
+  },
+  {
+    "revision": "5fe29c2c4d7d67861c24",
+    "url": "/ctf_task/static/js/main.c927ec28.chunk.js"
+  },
+  {
+    "revision": "7599f7f637d00b213a52",
+    "url": "/ctf_task/static/js/runtime-main.88be4eb7.js"
+  }
+]);
