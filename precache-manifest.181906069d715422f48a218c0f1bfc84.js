fad92cd780f4b27cd0f1433f5f47cb686f74ad53
Rogith-Ranger <rogithy@gmail.com>
2019-10-04 20:17:34 +0530

    Updates

---
+++ precache-manifest.181906069d715422f48a218c0f1bfc84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d71265f8d95b7a91c13e0caedfec76c",
+    "url": "/Rogith-Ranger/ctf_task/index.html"
+  },
+  {
+    "revision": "4f60d1df96f29dfb1596",
+    "url": "/Rogith-Ranger/ctf_task/static/css/main.61f6dcfd.chunk.css"
+  },
+  {
+    "revision": "c433f050a33f1962da19",
+    "url": "/Rogith-Ranger/ctf_task/static/js/2.041e56c1.chunk.js"
+  },
+  {
+    "revision": "4f60d1df96f29dfb1596",
+    "url": "/Rogith-Ranger/ctf_task/static/js/main.41ce0b7c.chunk.js"
+  },
+  {
+    "revision": "00ad81ae65405f766284",
+    "url": "/Rogith-Ranger/ctf_task/static/js/runtime-main.3f91a179.js"
+  }
+]);
